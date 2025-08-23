1b17b913ed53c0963231918b997e1e7b85df078d
abhishek chaudhary <abhishek0221997@gmail.com>
2025-08-23 17:59:15 +0530

    refactor: update app.js request handling and add package.json scripts

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express(); // create web server
 // request handler
-app.use((req,res)=>{
+app.use("/test",(req,res)=>{
     res.send("Hello from the server");
-})
+});
+app.use("/hello",(req,res)=>{
+    res.send("Hello mr. hacker");
+});
 app.listen(3001, () => {
     console.log("Server is running on port 3001");
 });
---
+++ work.md
@@ -0,0 +1,4 @@
+create a repo.
+intitalize a repo.
+install express.
+create a server.
