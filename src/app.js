b6cc2d855bc0b2570a09a8c0a146535b7592f7cf
abhishek chaudhary <abhishek0221997@gmail.com>
2025-08-23 17:29:27 +0530

    refactor: update server setup and request handling in app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
-const express=require('express');
-const app=express();// create web server
-app.listen(3000);
+const express = require("express");
+const app = express(); // create web server
+// request handler
+app.use((req,res)=>{
+    res.send("Hello from the server");
+})
+app.listen(3001, () => {
+    console.log("Server is running on port 3001");
+});
