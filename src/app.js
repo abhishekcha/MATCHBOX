08f223c5186370018f189cb57f1ed9e54cbed40a
abhishek chaudhary <abhishek0221997@gmail.com>
2025-08-23 22:32:58 +0530

    get post delete

--- src/app.js
+++ src/app.js
@@ -1,12 +1,35 @@
 const express = require("express");
 const app = express(); // create web server
 // request handler
-app.use("/test",(req,res)=>{
-    res.send("Hello from the server");
+// app.use("/hello/2",(req,res)=>{
+//     res.send("system hacked!! warning!!!");
+// });
+// app.use("/hello",(req,res)=>{
+//     res.send("Hello mr. hacker");
+// });/
+
+// this will match only get http method to /user
+app.get("/user", (req, res) => {
+  res.send({ name: "abhishek", age: 22, city: "delhi" });
 });
-app.use("/hello",(req,res)=>{
-    res.send("Hello mr. hacker");
+
+app.post("/user", (req, res) => {
+  res.send("data successfully saved");
 });
+
+app.delete("/user", (req, res) => {
+  res.send("data successfully deleted");
+});
+
+// this will match all the  http methods to /test
+app.use("/test", (req, res) => {
+  res.send("Hello from the server");
+});
+
+// app.use("/",(req,res)=>{
+//     res.send("hello abhishek");
+// });
+
 app.listen(3001, () => {
-    console.log("Server is running on port 3001");
+  console.log("Server is running on port 3001");
 });
--- work.md
+++ work.md
@@ -2,3 +2,5 @@ create a repo.
 intitalize a repo.
 install express.
 create a server.
+listen to port 3001.
+write logic to handle get post patch delete api calls and test them on postman
