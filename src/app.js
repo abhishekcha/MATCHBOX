62520a4f3bc473353e6506daec41a7d59f70a367
abhishek chaudhary <abhishek0221997@gmail.com>
2025-08-23 23:02:59 +0530

    refactor: update route handling in app.js and enhance work.md documentation

--- src/app.js
+++ src/app.js
@@ -1,34 +1,12 @@
 const express = require("express");
 const app = express(); // create web server
-// request handler
-// app.use("/hello/2",(req,res)=>{
-//     res.send("system hacked!! warning!!!");
-// });
-// app.use("/hello",(req,res)=>{
-//     res.send("Hello mr. hacker");
-// });/
+
 
 // this will match only get http method to /user
-app.get("/user", (req, res) => {
+app.get("/abc", (req, res) => {
   res.send({ name: "abhishek", age: 22, city: "delhi" });
 });
 
-app.post("/user", (req, res) => {
-  res.send("data successfully saved");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("data successfully deleted");
-});
-
-// this will match all the  http methods to /test
-app.use("/test", (req, res) => {
-  res.send("Hello from the server");
-});
-
-// app.use("/",(req,res)=>{
-//     res.send("hello abhishek");
-// });
 
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
--- work.md
+++ work.md
@@ -3,4 +3,8 @@ intitalize a repo.
 install express.
 create a server.
 listen to port 3001.
-write logic to handle get post patch delete api calls and test them on postman
+write logic to handle get post patch delete api calls and test them on postman.
+explore rounting and use of ?, +, (), * in the routes
+use of regex in routes /a/, /.*fly$/
+reading the query params in the routes
+read the dynamic routes
