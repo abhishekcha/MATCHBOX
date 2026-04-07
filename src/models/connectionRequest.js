4f870f6cf637ba27475337996afe33d991bd05e1
abhishek chaudhary <abhishek0221997@gmail.com>
2026-04-07 08:04:45 +0530

    fix: correct status value in connection request schema

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -9,11 +9,13 @@ const connectionRequestSchema = new mongoose.Schema(
     },
     toUserId: {
       type: mongoose.Schema.Types.ObjectId,
+      ref:"User",
+      required: true,
     },
     status: {
       type: String,
       enum: {
-        values: ["ignore", "interested", "accepted", "rejected"],
+        values: ["ignored", "interested", "accepted", "rejected"],
         message: `{VALUE} is incorrect status type`,
       },
     },
@@ -35,3 +37,4 @@ const ConnectionRequestModel = new mongoose.model(
   connectionRequestSchema
 );
 module.exports = ConnectionRequestModel;
+
