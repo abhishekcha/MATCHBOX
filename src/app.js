462a151139b0802d6fb51f2e13a4e4cd705710c3
abhishek chaudhary <abhishek0221997@gmail.com>
2026-04-07 08:05:26 +0530

    fix: add CORS middleware to enable cross-origin requests

--- src/app.js
+++ src/app.js
@@ -2,6 +2,15 @@ const express = require("express");
 const { connectDB } = require("./config/database");
 const app = express(); // create web server
 const cookieParser = require("cookie-parser");
+const cors = require('cors')
+
+const corsOptions = {
+  origin: "http://localhost:5173", // set your frontend origin
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+
 app.use(express.json()); // middleware to parse JSON request body
 app.use(cookieParser()); // middleware to parse cookies
 
