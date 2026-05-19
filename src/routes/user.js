2d38b1ea76744996bb0677780966998897741ed8
abhishek chaudhary <abhishek0221997@gmail.com>
2026-05-19 19:04:35 +0530

    fix: add pagination to feed API for improved data handling

--- src/routes/user.js
+++ src/routes/user.js
@@ -68,6 +68,10 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 userRouter.get("/feed", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
+
+    const page= parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
     const connectionRequests = await connectionRequest
       .find({
         // not same as video of namaste dev
@@ -85,7 +89,7 @@ userRouter.get("/feed", userAuth, async (req, res) => {
         { _id: { $nin: Array.from(hideUsersFromFeed) } },
         { _id: { $ne: loggedInUser._id } },
       ],
-    }).select(USER_SAFE_DATA);
+    }).select(USER_SAFE_DATA).skip(skip).limit(limit);
     res.send(users);
     //res.json({ data: connectionRequests });
   } catch (err) {
