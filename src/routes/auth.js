a4b5a64c404f7d08f39916c82b186f4f4e4029ad
abhishek chaudhary <abhishek0221997@gmail.com>
2026-05-19 19:04:26 +0530

    fix: format code for consistency and improve readability in auth routes

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,14 +1,13 @@
-const express=require("express");
-const authRouter=express.Router();
+const express = require("express");
+const authRouter = express.Router();
 
 const { validateSignUpData } = require("../utils/validation");
 const bcrypt = require("bcrypt");
 const { User } = require("../models/user");
 const jwt = require("jsonwebtoken");
 
-
-authRouter.post("/signUp",async(req,res)=>{
-    try {
+authRouter.post("/signUp", async (req, res) => {
+  try {
     validateSignUpData(req);
     const { firstName, lastName, emailId, password } = req.body;
     const passwordHash = await bcrypt.hash(password, 10);
@@ -18,15 +17,20 @@ authRouter.post("/signUp",async(req,res)=>{
       emailId,
       password: passwordHash,
     }); // create a new User document from request body
-    await user.save(); // save the document to the database
-    res.send("data added successfully!");
+    const savedUser = await user.save(); // save the document to the database
+    const token = await savedUser.getJWT();
+
+      res.cookie("token", token, {
+        expires: new Date(Date.now() + 8 * 3600000),
+      });
+    res.json({ message: "data added successfully!", data: savedUser });
   } catch (err) {
     res.status(400).send(" Error while saving data:" + err.message);
   }
 });
 
 authRouter.post("/login", async (req, res) => {
-   try {
+  try {
     const { emailId, password } = req.body;
 
     const user = await User.findOne({ emailId: emailId });
@@ -50,11 +54,11 @@ authRouter.post("/login", async (req, res) => {
   }
 });
 
-authRouter.post("/logout",async(req,res)=>{
-    res.cookie("token",null,{
-        expires:new Date(Date.now()),
-    });
-    res.send("logout successfully!");
+authRouter.post("/logout", async (req, res) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+  });
+  res.send("logout successfully!");
 });
 
-module.exports=authRouter;
+module.exports = authRouter;
